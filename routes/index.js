c3a65ee5d0ee5d0913585c8e07e37a9b88d4a651
Olegs Radcenko <olegs.radcenko@gmail.com>
2023-11-06 23:15:52 +0100

    Cyclic update

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,27 @@
 var express = require('express');
 var router = express.Router();
-const fs = require('fs');
-var path = require('path');
+const AWS = require("aws-sdk");
+const s3 = new AWS.S3()
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  const pictures = fs.readdirSync(path.join(__dirname, '../pictures/')).slice(0, 3);
+router.get('/', async function(req, res, next) {
+  var params = {
+    Bucket: process.env.CYCLIC_BUCKET_NAME,
+    Delimiter: '/',
+    Prefix: 'public/'
+  };
+  var allObjects = await s3.listObjects(params).promise();
+  var keys = allObjects?.Contents.map( x=> x.Key).slice(0,3);
+  const pictures = await Promise.all(keys.map(async (key) => {
+    let my_file = await s3.getObject({
+      Bucket: process.env.CYCLIC_BUCKET_NAME,
+      Key: key,
+    }).promise();
+    return {
+        src: Buffer.from(my_file.Body).toString('base64'),
+        name: key.split("/").pop()
+    }
+  }))
   res.render('index', { pictures: pictures, title: 'Express' });
 });
 
--- routes/pictures.js
+++ routes/pictures.js
@@ -2,39 +2,46 @@ var express = require('express');
 var router = express.Router();
 const fs = require('fs');
 var path = require('path');
+const AWS = require("aws-sdk");
+const s3 = new AWS.S3();
 
 /* GET pictures listing. */
-router.get('/', function(req, res, next) {
-  const pictures = fs.readdirSync(path.join(__dirname, '../pictures/'));
+router.get('/', async function(req, res, next) {
+  var params = {
+    Bucket: process.env.CYCLIC_BUCKET_NAME,
+    Delimiter: '/',
+    Prefix: 'public/'
+  };
+  var allObjects = await s3.listObjects(params).promise();
+  var keys = allObjects?.Contents.map( x=> x.Key);
+  const pictures = await Promise.all(keys.map(async (key) => {
+    let my_file = await s3.getObject({
+      Bucket: process.env.CYCLIC_BUCKET_NAME,
+      Key: key,
+    }).promise();
+    return {
+        src: Buffer.from(my_file.Body).toString('base64'),
+        name: key.split("/").pop()
+    }
+  }))
   res.render('pictures', { pictures: pictures});
 });
 
 // Route for serving a single picture by name
 router.get('/:pictureName', function(req, res, next) {
-  const pictureName = req.params.pictureName;
-  const picturePath = path.join(__dirname, '../pictures/', pictureName);
-
-  // Check if the file exists
-  if (fs.existsSync(picturePath)) {
-    // Serve the image file
-    res.sendFile(picturePath);
-  } else {
-    // If the file does not exist, send a 404 response
-    res.status(404).send('Picture not found');
-  }
+  res.render('pictureDetails', { picture: req.params.pictureName });
 });
 
-// router.get('/:pictureName', function(req, res, next) {
-//   res.render('pictureDetails', { picture: req.params.pictureName });
-// });
-
-
 //display a file from the request in the logs and save in folder
-router.post('/', function(req, res, next) {
-    const file = req.files.file;
-    fs.writeFileSync(path.join(__dirname, '../pictures/', file.name), file.data);
-    console.log(req.files);
-    res.end();
-  });
+router.post('/', async function(req, res, next) {
+  const file = req.files.file;
+  console.log(req.files);
+  await s3.putObject({
+    Body: file.data,
+    Bucket: process.env.CYCLIC_BUCKET_NAME,
+    Key: "public/" + file.name,
+  }).promise()
+  res.end();
+});
 
 module.exports = router;
